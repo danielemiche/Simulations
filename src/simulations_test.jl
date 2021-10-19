e672527a9ccc88dd93ae681770c81849929f82be
danielemiche <74101475+danielemiche@users.noreply.github.com>
2021-10-19 09:38:25 +0200

    cleaned code

--- src/simulations_test.jl
+++ src/simulations_test.jl
@@ -1,9 +1,15 @@
 using simulations 
 
 u = simulations.unif(100)
+
 for x in u 
     if x<0 || x>1
         throw(ErrorException("uniform realizations must be between 0 and 1"))
     end
 end
-e = simulations.inv_exp(u[1], 7)
+
+e = simulations.inv_exp(u, 7)
+
+
+
+

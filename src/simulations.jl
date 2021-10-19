33ea445cc5b964c6be3d788eba85e99f0267c687
danielemiche <74101475+danielemiche@users.noreply.github.com>
2021-10-19 10:12:09 +0200

    added comments

--- src/simulations.jl
+++ src/simulations.jl
@@ -1,10 +1,8 @@
 module simulations
 
 using StatsBase
-#using Base
 
-n = 10
-
+# Linear Congruential Generator with time in nanoseconds as random seed 
 function unif(n::Int = 1, m::Int = 2^32, a::Int = 214013, c::Int = 2531011; seed::Integer = 0)
     if seed == 0
         seed = UInt64(time_ns())#*sum(Array{UInt64}(undef, 10))
@@ -14,41 +12,28 @@ function unif(n::Int = 1, m::Int = 2^32, a::Int = 214013, c::Int = 2531011; seed
     sample = Float64[]
     for i = 1:n
         seed = (a * seed + c) % m
-        # seed = (Base.checked_add(Base.checked_mul(a, seed) + c)) % m #if using Int 
         push!(sample, seed/m)
     end
     sample        
 end
-# u = unif(n)
 
-# function inv_exp(x::Real, lambda::Real = 1)
-#     return -log(1-x)/lambda
-# end
 function inv_exp(x::Union{Vector{Float64}, Float64}, lambda::Real = 1)
     return -log.(1 .- x) ./ lambda
 end
 
+# sample exponential
 function rand_exp(n::Int = 1; lambda::Real = 1)
     u = unif(n)
     return inv_exp(u, lambda)
 end
-# inv_exp(u)
-
-# function Box_Muller_method(x::Float64, y::Float64)
-#     z_1 = sqrt(-2*log(x))*cos(2*π*y)    
-#     z_2 = sqrt(-2*log(y))*cos(2*π*x)
-#     return [z_1, z_2]
-# end
 
 function Box_Muller_method(x::Union{Vector{Float64}, Float64}, y::Union{Vector{Float64}, Float64})
     z_1 = sqrt.(-2 .*log.(x)) .*cos.(2 .*π .*y)    
     z_2 = sqrt.(-2 .*log.(y)) .*cos.(2 .*π .*x)
     return vcat(z_1, z_2)
 end
-# ui = unif(n)
-# uj = unif(n)
-# Box_Muller_method(ui, uj)
 
+# sample normal
 function rand_norm(n::Int = 1; mean::Real = 0, variance::Real = 1)
     m = n%2==0 ? n : n+1 
     u = unif(m)
@@ -56,6 +41,7 @@ function rand_norm(n::Int = 1; mean::Real = 0, variance::Real = 1)
     return variance .* z[1:n] .+ mean
 end
 
+# sample gamma 
 function Marsaglia_Tsang(n::Int, alpha::Real, beta::Real)
     seed = Int(time_ns())
     d = alpha-1/3
@@ -72,8 +58,8 @@ function Marsaglia_Tsang(n::Int, alpha::Real, beta::Real)
     end
     return sample ./ beta
 end
-# Marsaglia_Tsang(10, 2, 1)
 
+# sample beta using gamma
 function beta_from_gamma(alpha::Real, beta::Real, n::Int =1)
     g_1 = Marsaglia_Tsang(n, alpha, 1)
     g_2 = Marsaglia_Tsang(n, beta, 1)
@@ -83,8 +69,8 @@ function beta_from_gamma(alpha::Real, beta::Real, n::Int =1)
     end
     return b
 end
-# beta_from_gamma(10, 3, 1000)
 
+# sample Chinese Restaurant Process
 function CRP(n::Int, alpha::Real)
     clients = [1]
     for i=2:n
@@ -94,8 +80,8 @@ function CRP(n::Int, alpha::Real)
     end
     return clients 
 end
-# clients = CRP(1000, 100)
 
+# sample Dirichlet Process
 function beta_stick_breaking(n::Int, alpha::Real)
     probs = [beta_from_gamma(1, alpha)]
     for i = 1:n
@@ -104,7 +90,5 @@ function beta_stick_breaking(n::Int, alpha::Real)
     end
     return probs
 end
-
-# probs = beta_stick_breaking(1000, 10)
 
 end # module
